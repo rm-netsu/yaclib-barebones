90ea81a2934b3fad1854988e89476cf5995ad233
rm-netsu <netsu.code@gmail.com>
2024-10-26 04:40:10 +0300

    add cell render function customization to qrcode

--- qrcode.js
+++ qrcode.js
@@ -4,10 +4,30 @@
 	correction={'H'|'M'|'L'}
 	margin={number}
 	cellsize={number}>
+	cellfn={(x, y, c) => `<svg-element ... />`}
 </yac-qrcode>
 */
 
 import qrcode from 'qrcode-generator-es6'
+
+/** Sample function for square cells */
+export const squareFn = (x, y, c) => `<rect
+	width="${c}"
+	height="${c}"
+	x="${x}"
+	y="${y}"
+	fill="black"
+	shape-rendering="crispEdges"
+/>`
+
+/** Sample function for circle cells */
+export const circleFn = (x, y, c) => `<circle
+	r="${c/2}"
+	cx="${x +c/2}"
+	cy="${y +c/2}"
+	fill="black"
+	shape-rendering="crispEdges"
+/>`
 
 const sheet = new CSSStyleSheet()
 sheet.replaceSync('SVG { display: block; }')
@@ -17,16 +37,19 @@ const defaults = {
 	cellsize: 1,
 	url: document.baseURI,
 	correction: 'M',
+	cellfn: squareFn,
 }
 
 export default class HTMLQrCodeElement extends HTMLElement {
-	static observedAttributes = ['margin', 'cellsize', 'url', 'correction']
+	static observedAttributes = ['margin', 'cellsize', 'url', 'correction', 'cellfn']
 	#state = { ...defaults }
 	
 	constructor() {
 		super()
 		this.attachShadow({ mode: 'open' })
 		this.shadowRoot.adoptedStyleSheets = [sheet]
+		
+		this.render()
 	}
 	
 	attributeChangedCallback($a, oldValue, newValue) {
@@ -42,6 +65,9 @@ export default class HTMLQrCodeElement extends HTMLElement {
 			case 'correction':
 				this.#state[$a] = newValue.toUpperCase()
 			break
+			case 'cellfn':
+				this.#state[$a] = new Function(`return ${newValue}`)()
+			break
 		}
 		
 		this.render()
@@ -52,10 +78,29 @@ export default class HTMLQrCodeElement extends HTMLElement {
 		qr.addData(this.#state.url)
 		qr.make()
 		
-		this.shadowRoot.innerHTML = qr.createSvgTag({
-			margin: this.#state.margin,
-			cellSize: this.#state.cellsize
-		})
+		const moduleCount = qr.getModuleCount()
+		
+		const { margin, cellsize, cellfn } = this.#state
+		
+		const sideLength = moduleCount * cellsize + margin*2
+		
+		const cells = []
+		for(let Y = 0; Y < moduleCount; ++Y)
+		for(let X = 0; X < moduleCount; ++X)
+		if(qr.isDark(Y, X))
+			cells.push(cellfn(X * cellsize + margin, Y * cellsize + margin, cellsize))
+		
+		const svg = `<svg
+			version="1.1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:xlink="http://www.w3.org/1999/xlink"
+			viewBox="0 0 ${sideLength} ${sideLength}"
+			preserveAspectRatio="xMinYMin meet"
+		><rect width="100%" height="100%" fill="white" x="0" y="0" />${
+			cells.join('')
+		}</svg>`
+		
+		this.shadowRoot.innerHTML = svg
 	}
 }
 customElements.define('yac-qrcode', HTMLQrCodeElement)
