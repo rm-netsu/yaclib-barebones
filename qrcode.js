d604f2b2151b63d8de82662707b5652fa7752a2b
rm-netsu <netsu.code@gmail.com>
2024-11-27 18:23:42 +0300

    fix qr code background

--- qrcode.js
+++ qrcode.js
@@ -34,7 +34,7 @@ sheet.replaceSync(
 `.wrap { position: relative; }
 SVG { display: block; }
 .bg {
-	background: --var(bg, white);
+	background: var(--bg, white);
 	position: absolute; left: 0; top: 0; bottom: 0; right: 0; z-index: -1;
 }
 .favicon {
